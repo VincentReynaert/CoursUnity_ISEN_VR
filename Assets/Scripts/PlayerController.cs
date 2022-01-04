954b24d21566be16b1cfa070773cb3df6dc3358d
Vincent Reynaert <v.vincent59230@laposte.net>
2022-01-04 21:22:00 +0100

    init

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float _speed = 10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(_speed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalMove = Input.GetAxis("Horizontal");
+        float verticalMove = Input.GetAxis("Vertical");
+        transform.position += new Vector3(horizontalMove, 0, verticalMove).normalized * _speed;
+    }
+}
